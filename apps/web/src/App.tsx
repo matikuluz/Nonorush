f6f707baa2cfc49492d9eb4908c419a88e661baf
hellomathias <mathiasschembri@gmail.com>
2025-11-15 18:30:11 +0100

    chore: initial project setup

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Nonorush</h1>;
+}
+
+export default App;
---
+++ apps/web/src/lib/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+
+const cfg = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID
+};
+
+export const app = initializeApp(cfg);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+if (import.meta.env.VITE_USE_EMULATORS === "true") {
+  connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
+  connectFirestoreEmulator(db, "localhost", 8080);
+}
